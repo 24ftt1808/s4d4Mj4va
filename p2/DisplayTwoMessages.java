a86ede71577826e6b100df686c948728e489d218
24ftt1808 <24ftt1808@student.pb.edu.bn>
2025-08-07 16:28:00 +0800

    Create DisplayTwoMessages.java

---
+++ p2/DisplayTwoMessages.java
@@ -0,0 +1,7 @@
+public class DisplayTwoMessages {
+    public static void main(String[] args) {
+        System.out.println("Welcome To AD2204 Introduction To Programming");
+        System.out.println("This is Practical 2");
+    }
+    
+}
