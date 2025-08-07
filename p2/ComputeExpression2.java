3ad843f6980386e63a8357a45aae8335cfadc200
24ftt1808 <24ftt1808@student.pb.edu.bn>
2025-08-07 16:41:31 +0800

    Update ComputeExpression2.java

--- p2/ComputeExpression2.java
+++ p2/ComputeExpression2.java
@@ -1,7 +1,7 @@
 public class ComputeExpression2 {
     public static void main(String[] args) {
         double result = (7.5 * 2.5 - 1.5 * 3) / (4-2.5);
-        System.out.println("The result is" +result);
+        System.out.println("The result is " +result);
         
     }
     
