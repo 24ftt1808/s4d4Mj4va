1358cf7add3ce1fa642505f62c776d702275bf4a
24ftt1808 <24ftt1808@student.pb.edu.bn>
2025-08-07 16:33:04 +0800

    Create ComputeExpression2.java

---
+++ p2/ComputeExpression2.java
@@ -0,0 +1,8 @@
+public class ComputeExpression2 {
+    public static void main(String[] args) {
+        double result = (7.5 * 2.5 - 1.5 * 3) / (4-2.5);
+        System.out.println("The result is" +result);
+        
+    }
+    
+}
